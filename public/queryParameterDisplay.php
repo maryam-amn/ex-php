a96c03c0bf46a3bcb614a8f63cafeb83705451d9
maryam-amn <Maryam.Aman@jobtrek.ch>
2025-03-25 10:29:00 +0100

    feat: the queryParameterDisplay excercice is done

--- public/queryParameterDisplay.php
+++ public/queryParameterDisplay.php
@@ -2,6 +2,26 @@
 /**
  * Get the values from the GET parameters with filter_input function
  */
+$errors = [];
+$name = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$age = filter_input(INPUT_GET, 'age', FILTER_SANITIZE_NUMBER_INT);
+$name = ucfirst($name);
+
+if ($name === null) {
+    array_push($errors, 'Name is required');
+}
+if ($name === false) {
+    array_push($errors, 'The name is invalit, plese use only letters and numbers');
+}
+if (mb_strlen($name) < 2) {
+    array_push($errors, 'Missing name');
+}
+if ($age === null) {
+    array_push($errors, 'Missing age');
+}
+if ($age === false) {
+    array_push($errors, 'Age is invald, plese use only numbers');
+}
 
 ?>
 
@@ -15,6 +35,22 @@
     <title>URL query parameters</title>
 </head>
 <body>
+
+
+<?php if (count($errors) > 0) { ?>
+<h1>No query parameters found</h1>
+    <p>Erreurs :</p>
+    <?php foreach ($errors as $error) { ?>
+  <li><?= $error ?></li>
+
+    <?php } ?>
+<?php } else { ?>
+
+<h1> <?= $name?> is <?= $age?> years old </h1>
+
+<?php } ?>
+
+
 
 <!-- Display parameters here in a h1 tag -->
 
